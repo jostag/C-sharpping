8a518a85bd110f5bd807332ab633f27289deeea5
jostag <35642154+jostag@users.noreply.github.com>
2018-01-21 12:57:35 -0500

    Add files via upload

---
+++ Pingsweep.ps1
@@ -0,0 +1,11 @@
+﻿$first = 0
+$last = 255
+$sub_net = Read-Host "Please enter the first 3 octets of the IP address"
+while ($first -le $last) {
+    $IP = "$sub_net.$first" 
+    $test_connection = Test-Connection -ComputerName $IP -Count 1 -Quiet
+    if ($test_connection){ 
+        Write-Host "$IP" -ForegroundColor Green
+    }
+    $first++
+}
